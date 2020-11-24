7cc46a2e220a0f42ecc87ca7b9925107a7ad8810
mark-shumskiy <72410849+mark-shumskiy@users.noreply.github.com>
2020-11-23 17:34:23 -0800

    Update teen_pregnancy.R

--- scripts/teen_pregnancy.R
+++ scripts/teen_pregnancy.R
@@ -1,10 +1,16 @@
 library("tidyverse")
 
-teen_preg <- (Teen_Pregnancy_2016) %>% 
-  group_by(sex_ed) %>% 
-  summarize(state, state_id, pregnancy_rate, sex_ed)
+teen_pregnancy_2016 <- read.csv("data/teen_preg_2016.csv")
 
-ggplot(teen_preg) +
+teen_preg <- (teen_pregnancy_2016) %>% 
+  group_by(sex_ed) %>% 
+  select(state, state_id, pregnancy_rate, sex_ed)
+
+teen_preg_chart<- ggplot(teen_preg) +
 geom_col(mapping = aes(
-    x = state_id, y = pregnancy_rate, fill = sex_ed)) +
+    x = reorder(state_id, pregnancy_rate), y = pregnancy_rate, fill = sex_ed)) +
+  labs(x = "State", y = "Pregnancy Rate", fill = "Level of Sexual Education") +
+  ggtitle("United States 2016 Teen Pregnancy by State ") +
+  scale_fill_discrete(labels = c("Not Required", "Required", 
+                      "Required & Medically Accurate")) +
   coord_flip() 
