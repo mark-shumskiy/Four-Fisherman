01a3c537733afe43d9ac218db6eb2e3192c96d64
mark-shumskiy <72410849+mark-shumskiy@users.noreply.github.com>
2020-11-23 17:33:21 -0800

    Update aggregate_table.R

--- scripts/aggregate_table.R
+++ scripts/aggregate_table.R
@@ -19,19 +19,18 @@ abortions <- filter(
 teen_abort <- full_join(abortions, requirement_level)
 teen_abort <- teen_abort %>% 
   mutate(state = tolower(state))
-View(teen_abort)
+
 contraceptives <- contraceptives %>% 
   mutate(state = tolower(state))
+
 teen_preg <- teen_preg %>% 
   mutate(state = tolower(state))
-View(teen_preg)
 
 # combine abortions with teen pregnancies
 teen_preg_and_abort <- na.omit(full_join(teen_preg, teen_abort))
 summary_info <- na.omit(full_join(teen_preg_and_abort, contraceptives))
-View(summary_info)
 
 # group the data by sex ed category
-summary_info_grouped <- summary_info %>% 
+aggregate_table <- summary_info %>% 
   group_by(sex_ed) %>% 
-  summarise(state_id, sex_ed, abortionrate, pregnancy_rate, total_contraceptives)
+  select(state_id, sex_ed, abortionrate, pregnancy_rate, total_contraceptives)
