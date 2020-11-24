978f2c433a95b770c3688d9c40a0f4c4674bf902
mark-shumskiy <72410849+mark-shumskiy@users.noreply.github.com>
2020-11-23 17:33:48 -0800

    Update teen_abortion.R

--- scripts/teen_abortion.R
+++ scripts/teen_abortion.R
@@ -2,46 +2,45 @@
 
 library("tidyverse")
 library("ggplot2")
-install.packages("maps")
-install.packages("mapproj")
 library("maps")
 library("mapproj")
 
-# read in the data about the three different categories of states
-# and the data about abortion rates by state
 requirement_level <- read.csv("data/state_level_requirement.csv")
 abortions <- read.csv("data/NationalandStatePregnancy.csv")
-
-# filter out all years except 2016 from abortions
 abortions <- filter(
   abortions,
   year == "2016"
 )
-
-# combine the two tables
 teen_abort <- full_join(abortions, requirement_level)
+
 teen_abort <- teen_abort %>% 
   mutate(state = tolower(state))
-View(teen_abort)
 
-# group by requirement level
-# teen_abort_by_level <- teen_abort %>% 
-# group_by(requirement_level) %>% 
-# summarise(state_id, abortionratelt20, requirement_level)
-
-# make a chart
 state_shape <- map_data("state") %>% 
   rename(state = region) %>% 
   full_join(teen_abort, by="state")
-View(state_shape)
 
-ggplot(state_shape) +
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(), # remove axis lines
+    axis.text = element_blank(), # remove axis labels
+    axis.ticks = element_blank(), # remove axis ticks
+    axis.title = element_blank(), # remove axis titles
+    plot.background = element_blank(), # remove gray background
+    panel.grid.major = element_blank(), # remove major grid lines
+    panel.grid.minor = element_blank(), # remove minor grid lines
+    panel.border = element_blank() # remove border around plot
+  )
+
+teen_abort_map <- ggplot(state_shape) +
   geom_polygon(
     mapping = aes(
-      x = long, y = lat, group = group, fill = abortionratelt20),
+      x = long, y = lat, group = group, fill = abortionrate),
     color = "white",
     size = .1
   ) +
   coord_map() +
+  blank_theme +
   scale_fill_continuous(low = "slategray2", high = "mediumorchid2") +
-  labs(fill = "Abortion Rate")
+  labs(fill = "Abortion Rate") +
+  ggtitle("United States 2016 Teen Abortion")
